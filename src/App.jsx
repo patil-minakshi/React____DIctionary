d919ac7afbfe7614e585aa51190c03837e4ddbd1
Minakshi Patil <pminu1972000@gmail.com>
2024-03-15 19:21:45 +0530

    css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import DictWords from "./Showdict"
-
+ 
 function App() {
  
 
--- src/Showdict.jsx
+++ src/Showdict.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-
 function DictWords() {
     const [words, setwords] = useState([])
     const [key, setkey] = useState('')
+
+
 
 
     async function Fetchwords(keyword) {   // on go button click run 
@@ -27,58 +28,75 @@ function DictWords() {
         setkey(e.target.value)
     }
 
+    function ListenIt(ad) {
+
+        window.open(ad, "_blank")
+    }
+
     return (
         <>
             <div className="Nav_container">
                 <div className="heading">
-                    Dictionary
+                   <h2 className="name">Dictionary</h2>
                 </div>
-               <div className="Search_container">
-                <div className="Right_search">
-                 
-                <input type="text" className="Input" onChange={Searchkeyword} placeholder="Search word" />
-                <button type="button" className="Go_btn" onClick={() => Fetchwords(key)}  >GO</button>
-               </div>
+                <div className="Search_container">
+                    <div className="Right_search">
+
+                        <input type="text" className="Input" onChange={Searchkeyword} placeholder="Search word" />
+                        <button type="button" className="Go_btn" onClick={() => Fetchwords(key)}  >GO</button>
+                    </div>
 
                 </div>
-               
+
             </div>
-                <div >
-                    {words.map((wd , index) => (
-                        <>
+            <div >
+                {words.slice(0,2).map((wd, index) => (
+                    <>
                         <div key={index} className="main_div">
                             <div className="Word_div">
-                            <h1>{wd.word}</h1>
+                                <h2>{wd.word}</h2>
 
-                            <p>link</p>
+                                {wd.phonetics &&  (
+                                    <p  className="audio" onClick={() => ListenIt(wd.phonetics[0].audio)}>Listen It</p>
+                                )}
+
                             </div>
-                            
+
                             <h3> Definations </h3>
+                            <div className="Defination_main">
                             <div className="Defination_div">
-                            {wd.meanings.map((mean ,index) => (    
-                                   <p key= {index}>    {mean.definitions[0].definition}    </p>
-            
-                            ))}
-                              </div>
-                  </div>
-                           
-                            <h3 >
-                                examples
-                            </h3>
-                            {wd.meanings.map((mn) => mn.definitions.map((def , index) => (
-                                 <div key={index} className="Defination_div">
-                              <p>{def.definition}</p>
-                              </div>
-                                 
-                            )))}
-                           
+                                {wd.meanings.map((mean, index) => (
+                                   
+                                    <p key={index}> {mean.definitions[0].definition}  </p>
+
+                                ))}
+                            </div>
+                            </div>
+                        </div>
+
+                        <h3 >
+                            examples
+                        </h3>
+                        {wd.meanings.map((mn) => mn.definitions.map((def, index) => (
+                            <div key={index} className="Example_div">
+                                <p>{def.definition}</p>
+                            </div>
+
+                        )))}
 
 
-                        </>
-                    ))}
-                    
-                </div>
-             
+
+                    </>
+                ))}
+
+            </div>
+
+            <footer>
+                <div className="Line"> made with Love by web developer</div>
+
+
+            </footer>
+
         </>
     )
 }
